cdaaad9e02c9fefbaf37a366bcb2361d164ff8b3
jimrothstein <jimrothstein@gmail.com>
2021-05-22 23:06:13 -0700

    wip

--- Rscripts/0030_simple_render.R
+++ Rscripts/0030_simple_render.R
@@ -13,10 +13,12 @@ Sys.time()
 
 file  <-  "~/code/try_things_here/rmd/00002_glue.Rmd"
 
+
+# getwd() - need to careful;   run as script it thinks Rscripts/ is wd
 rmarkdown::render(file,
 
                   output_format=c("html_document",    # if want both
                                   "md_document",
                                   "pdf_document"),
-                  output_dir = "out",
+                  output_dir = "~/code/try_things_here/out",
                   output_file = "out")
