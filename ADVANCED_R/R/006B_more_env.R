5f1600241a427659e5223c188e9fe6690d57b376
jimrothstein <jimrothstein@gmail.com>
2021-06-29 20:51:35 -0700

    wip

---
+++ ADVANCED_R/R/006B_more_env.R
@@ -0,0 +1,43 @@
+library(rlang)
+library(magrittr)
+
+
+f  <- function(x) {
+				env_print(current_env())
+}
+f(x)
+
+
+g  <- function(x) {
+#				f_list  <- (
+				env_print(current_env())
+}
+g(x)
+m  <- function(n){
+				pryr::promise_info(n)
+				env_print(current_env())
+}
+m(n)
+h  <- function(n){
+				n  <- 75
+				env_print(current_env())
+				cat("n = ",n,"\n") # not 100?
+				print("=====")
+				print("=====")
+				{k  <- function(n) {
+					n  <- 50
+					env_print(current_env())}
+				}
+				n  <- 25
+				k(n)
+
+			}
+
+n  <- 100
+h(n)
+n
+
+n %>% g() %>% f()
+
+# where am I:
+env_print(current_env())
---
+++ ADVANCED_R/my_old_code/025.2.1_nest.R
@@ -0,0 +1,7 @@
+df <- data.frame(name=c("a","b","b","c","c","c"), count=c(1,2,3,1,2,3))
+df
+library(tidyr)
+new <- df %>% nest()
+new
+new$data
+new$data[[1]]
