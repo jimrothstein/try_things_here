a8106bd421106b6ad7f9220732bc2601849710bd
jimrothstein <jimrothstein@gmail.com>
2023-02-19 16:09:51 -0800

    40_rrapply   review basic rrapply with oceania

--- tidyR_JSON_nest/40_rrapply_examples.R
+++ tidyR_JSON_nest/40_rrapply_examples.R
@@ -5,17 +5,19 @@
 ##          RRAPPLY::
 # --------------------
 
+##  use give.attr = F (cleaner output)
+##  list.len = 3
+##  max.depth = 1 or 2
+##  use listenviewer::jsonedit, even though no JSON
 ##
 library(rrapply)
+library(listviewer)
 data(package = "rrapply")
 
 # ----------------------------------------------------------
 str(pokedex , list.len=3)
 str(renewable_energy_by_country, list.len=3)  
 str(renewable_energy_by_country$World$Oceania, list.len=3)
-
-##  give.attr=F (cleaner):w
-##  
 str(renewable_energy_by_country$World$Oceania, list.len=3, give.attr = F)
 # ----------------------------------------------------------
 
@@ -23,8 +25,11 @@ str(renewable_energy_by_country$World$Oceania, list.len=3, give.attr = F)
 #
 # ------- pop off 2 levels, oceania & oceania[[1]] = ANZ ---------------------
 oceania  <- renewable_energy_by_country$World$Oceania
+listviewer::jsonedit(oceania)
 str(oceania, list.len=4 , give.attr = F)
 
+
+# ----------------------------------  ASIDE -------------------------------------
 # 1 more level
 str(australia_new_zealand  <- oceania[[1]], list.len=4, give.attr = F) 
 australia_new_zealand
@@ -37,12 +42,13 @@ is.atomic(u)
 # NOTE:  as_tibble for australia_new_zealan
 t  <- as_tibble(australia_new_zealand)
 t
+# -----------------------------------------------------------------------
 
 
 
 # ---------    oceania & rrapply -------------------------------------------------
-##  Cocos Islands has no number, prune will drop 
-##  give.attr=F much cleaner w/o attributes
+##  Cocos Islands has no number, in fact, is logical:NA;    
+##  prune will keep numberic rows, drop others 
 rrapply::rrapply(oceania,
                 f = \(x) x,
                 classes = "numeric",
@@ -50,24 +56,33 @@ rrapply::rrapply(oceania,
 ) |> str(list.len=3, give.attr=F)                 # as   
 
 
-#  replace logical to 0 for countries with missing values.
+#  replace logical to 0 for countries with missing values (NA), but do not drop
 rrapply::rrapply(oceania,
                 f = \(x) 0,
                 classes = "logical",
                 how  = "replace"
 ) |> str(list.len=3, give.attr=F)                 # as   
 
-## prune , flatten (to double[])
+
+## prune , flatten (to named atomic vector double[])
 rrapply::rrapply(oceania,
                 classes = "numeric",
                 how  = "flatten"
-) |> head(10)
+) 
 
-## Beautiful, returns double[]
+typeof(rrapply::rrapply(oceania,
+                classes = "numeric",
+                how  = "flatten"
+))
+
+
+
+## Beautiful, z is list, str shows contents named double[]
 z  <- rrapply::rrapply(oceania,
                 classes = "numeric",
                 how  = "flatten",
                 options = list(namesep=".", simplify = F)
 
 ) |> str(list.len=10, give.attr=F) 
-is.atomic(z)
+
+
