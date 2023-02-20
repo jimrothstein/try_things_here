7ab1276b34114bda5b174339bf42d3144dc6cf1d
jimrothstein <jimrothstein@gmail.com>
2023-02-20 03:05:07 -0800

    wip

--- BASE/0205_options_sessioninfo.R
+++ BASE/0205_options_sessioninfo.R
@@ -10,3 +10,30 @@
 #	.Options[grep('nvimcom || calendR || dplyr', names(.Options))]
 
 }
+
+# ------------ Change option, then reset---------------
+# SPELLING!     getOption (singular) but options (plural)
+#
+# Get option value
+old  <- getOption("digits")
+old
+print(pi)
+# [1] 3.1416
+
+
+# change
+options("digits"=20)
+print(pi)
+# [1] 3.141592653589793116
+
+# change back
+options("digits" = old)
+print(pi)
+# [1] 3.1416
+
+# -------------------------------------------------------
+#
+#
+z  <- utils::sessionInfo()
+z[["running"]]
+# [1] "GalliumOS 3.1"
