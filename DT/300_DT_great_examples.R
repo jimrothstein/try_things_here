be4610c7b4ee20590f7bcd4f76ff43224bf3f396
jimrothstein <jimrothstein@gmail.com>
2023-08-15 19:39:20 -0700

    wip

--- DT/300_DT_great_examples.R
+++ DT/300_DT_great_examples.R
@@ -3,21 +3,3 @@
 ## ------------------------
 #
 library(data.table)
-
-dt  <- data.table(mtcars)
-
-
-##  
-##  Generate arbitrary number of duplicate rows based upon value in col 
-##
-
-dt[
-   ,  list(1:5)
-   ,  by = "cyl"]
-
-
-
-
-dt[
-   ,  list(seq_len(cyl))
-   ,  by = "cyl"]
