978292b0b5363f7766f4f094af5c7e5f79c2b880
jimrothstein <jimrothstein@gmail.com>
2024-06-14 17:44:19 -0700

    wip

---
+++ sdtm_checks_PLAY/020_sdtmchecksmeta_PLAY.R
@@ -0,0 +1,21 @@
+#  sdtmchecks - practice area
+library(sdtmchecks)
+library(testthat)
+
+### PURPOSE:  Understand R code for sdtmchecksmeta 
+
+### For understanding one check function, see:  020_
+
+### shortcut for 
+meta = sdtmchecksmeta
+
+names(sdtmchecksmeta)
+
+#
+
+sdtmchecksmeta[1:5, 1]
+sdtmchecksmeta[1:5, c("check")]
+sdtmchecksmeta[1:5, c("fxn_in")]
+sdtmchecksmeta[1:5, c("pdf_return")]
+
+sdtmchecksmeta[1:5, c(1,2,3,4,8, 11)
