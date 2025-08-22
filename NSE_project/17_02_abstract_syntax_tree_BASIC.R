387e83832cee30583cf963dd64c030f43276e9a4
jimrothstein <jimrothstein@gmail.com>
2025-08-21 23:33:42 -0700

    wip

--- NSE_project/17_02_abstract_syntax_tree_BASIC.R
+++ NSE_project/17_02_abstract_syntax_tree_BASIC.R
@@ -126,6 +126,58 @@ lobstr::ast(f(!!z))   # evaluates !
 ## why not evaluate?
 lobstr::ast(
   f(eval(z)) # does not evaluate !
-)
+)##### parse
+ast(q1)  # appears as q1
+
+
+ast - more advanced, lobstr, lobstr + injection  EXPAIN!
+
+```r
+## Compare
+x = quote(a + b + c) # call
+
+q1 = lobstr::ast(x)   
+q1  # looks like just x 
+
+typeof(q1) # character    ??
+typeof(x)  # language 
+
+str(q1)  # 'lobstr_raw' chr  followed by terminal codes (will mess up printing) 
+str(x)  # language a + b + c
+
+lobstr::ast(!!q1)  # structure of terminal codes; class = "lobstr_raw"
+lobstr::ast(!!x)   # ast diagram of x
+
+
+
+
+q2=parse(text=q1) # expression(+ a b)
+parse(text=quote(a+b)) # expression(+ a b) (same)
+
+
+
+kexpression(+
+a, b)
+> x3=parse(text="quote(a+b)")
+> typeof(x3)
+[1] "expression"
+
+cex3 <- c("NULL", "1", "1:1", "1i", "list(1)", "data.frame(x = 1)",
+  "pairlist(pi)", "c", "lm", "formals(lm)[[1]]",  "formals(lm)[[2]]",
+  "y ~ x","expression((1))[[1]]", "(y ~ x)[[1]]",
+  "expression(x <- pi)[[1]][[1]]",
+  "quote(2+3)")
+
+cex3
+
+lex3 <- sapply(cex3, function(x) eval(str2lang(x)))             ## list, 15
+
+mex3 <- t(sapply(lex3,
+                 function(x) c(typeof(x), storage.mode(x), mode(x), is.language(x))))
+mex3
+dimnames(mex3) <- list(cex3, c("typeof(.)","storage.mode(.)","mode(.)"))
+mex3
+```
+
 
 
